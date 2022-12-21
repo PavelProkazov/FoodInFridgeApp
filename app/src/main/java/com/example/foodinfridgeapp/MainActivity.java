9cb62cbd33ae9645c7e6b4e0665f6da9e731910b
paprokazov@gmail.com <Zmd24aWasd!a@s>
2022-12-22 02:07:51 +0500

    f1

--- app/src/main/java/com/example/foodinfridgeapp/MainActivity.java
+++ app/src/main/java/com/example/foodinfridgeapp/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.foodinfridgeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button scanButton;
+    private Button addProductButton;
+    private EditText foodList_edit;
+    private TextView textFoodList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        scanButton = findViewById(R.id.ScanButton);
+        addProductButton = findViewById(R.id.addProductButton);
+        foodList_edit = findViewById(R.id.FoodListEditText);
+        textFoodList = findViewById(R.id.TextFoodList);
+
+
+
+
     }
+
+
 }
