261987e69a16eabc171d9a16e30222ecd9612d82
paprokazov@gmail.com <Zmd24aWasd!a@s>
2023-08-18 15:48:42 +0300

    f2

---
+++ app/src/main/java/com/example/foodinfridgeapp/Food.java
@@ -0,0 +1,35 @@
+package com.example.foodinfridgeapp;
+
+
+public class Food {
+
+    private String fName;
+    private String fData;
+    private String fPhoto;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getfData() {
+        return fData;
+    }
+
+    public void setfData(String fData) {
+        this.fData = fData;
+    }
+
+    public String getfPhoto() {
+        return fPhoto;
+    }
+
+    public void setfPhoto(String fPhoto) {
+        this.fPhoto = fPhoto;
+    }
+
+
+}
--- app/src/main/java/com/example/foodinfridgeapp/MainActivity.java
+++ app/src/main/java/com/example/foodinfridgeapp/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.foodinfridgeapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.foodinfridgeapp.ocrreader.OcrCaptureActivity;
 
 import java.util.Objects;
 
@@ -23,14 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         scanButton = findViewById(R.id.ScanButton);
+
         addProductButton = findViewById(R.id.addProductButton);
         foodList_edit = findViewById(R.id.FoodListEditText);
         textFoodList = findViewById(R.id.TextFoodList);
 
+    }
 
 
+
+    public void startScanText(View v){
+        Intent intent = new Intent(this, OcrCaptureActivity.class);
+        startActivity(intent);
 
     }
+
 
 
 }
